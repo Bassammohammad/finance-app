9e0638f12250744c67cc076c81f267601f3feb40
Bassammohammad <bsmbsm2001@gmail.com>
2025-01-22 15:48:35 +0100

    updating for a responsive User Interface

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -27,7 +27,7 @@ export const DataGrid = () => {
   }
 
   return (
-    <div className="grid grid-cols-2  lg:grid-cols-3 gap-8 pb-2 mb-8">
+    <div className="grid grid-cols-1  lg:grid-cols-3 gap-8 pb-2 mb-8">
       <DataCard
         title="Remaining"
         value={data?.remainingAmount}
